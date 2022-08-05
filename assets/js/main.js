afb8146f8fe1f6372c24ea90148660775e8c8af5
abdallah-sala7 <abdallah.salah.13811@gmail.com>
2022-08-05 17:30:29 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let navsBtn = document.querySelectorAll('nav ul li');
+
+// add event listener to navs
+
+navsBtn.forEach(e =>{
+    e.addEventListener('click', ()=>{
+        navsBtn.forEach(e=>{
+            e.classList.remove('active');
+        })
+        e.classList.add('active');
+    })
+})
