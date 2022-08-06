897267306141fd927eecb26246424ce9db18b354
abdallah-sala7 <abdallah.salah.13811@gmail.com>
2022-08-06 16:35:57 +0200

    add portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,51 @@ navsBtn.forEach(e =>{
     })
 })
 
-new GitHubCalendar(".calendar", "abdallah-sala7");
+// type writer effect
+const textDisplay = document.querySelector('.span-skils')
+const phrases = ['frontend developer', 'android developer', 'reactjs developer.']
+let i = 0
+let j = 0 
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop () {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join('')
+
+  if (i < phrases.length) {
+
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j])
+      j++
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if(isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j])
+      j--
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i === phrases.length) {
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 -50) + 50
+  const normalSpeed = Math.random() * (300 -200) + 200
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time)
+}
+
+loop()
